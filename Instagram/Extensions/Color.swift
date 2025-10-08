0a88815a5d43d21d9a8bd75f752abfd3c6be6436
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-10-08 20:19:44 -0300

    feat: theme

---
+++ Instagram/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  Instagram
+//
+//  Created by Luis Filipe Pedroso on 08/10/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("Accent")
+    let background = Color("Background")
+    let border = Color("Border")
+    let like = Color("Like")
+    let link = Color("Link")
+    let primaryText = Color("PrimaryText")
+    let secondaryText = Color("SecondaryText")
+    let surface = Color("Surface")
+}
