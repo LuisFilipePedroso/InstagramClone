24e46d09469a7a94d300a3d34035badfadfbc743
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-10-08 19:31:13 -0300

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Luis Filipe Pedroso on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
