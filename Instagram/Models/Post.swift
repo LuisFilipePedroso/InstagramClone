fd320e519fcadab6f04d96dbcb68f3bca80ed978
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-10-14 20:08:12 -0300

    feat: add reposts and sends field

--- Instagram/Models/Post.swift
+++ Instagram/Models/Post.swift
@@ -12,18 +12,20 @@ struct PostResponse: Codable {
     let next: Int?
 }
 
-struct Post: Codable {
-    let id: Int
+struct Post: Codable, Identifiable {
+    let id: String
     let username: String
     let userAvatar, imageURL: String
     let caption: String
     let likes: Int
+    let reposts: Int
+    let sends: Int
     let timestamp: Date
     let comments: [Comment]
 
     enum CodingKeys: String, CodingKey {
         case id, username, userAvatar
         case imageURL = "imageUrl"
-        case caption, likes, timestamp, comments
+        case caption, likes, reposts, sends, timestamp, comments
     }
 }
