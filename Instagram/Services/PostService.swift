27fa335297bf122ab403ec247b18f28fa9fca6b3
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-10-14 20:08:17 -0300

    feat: create post service

--- Instagram/Services/PostService.swift
+++ Instagram/Services/PostService.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class PostDataService {
+    
+    func getPosts(page: Int) async throws -> PostResponse {
+        let postsUrl = Configuration.url(for: .posts)
+        var components = URLComponents(string: postsUrl.absoluteString)
+        components?.queryItems = [
+            URLQueryItem(name: "_page", value: "\(page)"),
+            URLQueryItem(name: "_per_page", value: "10")
+        ]
+        
+        guard let url = components?.url else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+//        let response = try decoder.decode([Post].self, from: data)
+        let response = try decoder.decode(PostResponse.self, from: data)
+        return response
+    }
+}
